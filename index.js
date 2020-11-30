bc551f16b0ef9f57dad407548f576418261a83cb
abhachaudhary <abha.bittu@gmail.com>
2020-11-29 21:03:47 -0500

    change for table

--- index.js
+++ index.js
@@ -1,15 +1,35 @@
-let COVID_URL = "https://e51vfb2s30.execute-api.us-east-1.amazonaws.com/Dev/getcovidhospitals";
+var API_URL = "https://1399n7dte4.execute-api.us-east-1.amazonaws.com/dev/getcovidhospitals"
+$('#submitButton').on('click',function(){
+    console.log($("#zipcode").val());
+$.ajax({
+    type: 'GET',
+    url:API_URL,
+    data: {ZipCode: $("#zipcode").val()},
+    contentType:"application/json",
+    Origin: "*",
 
-//Fetching the World Data
-fetch(COVID_URL, {
-    "method": "GET",
-})
-.then(response=>response.json())
-.then(data=>{
-    console.log(data);
-    HospitalId.innerHTML = data.Items[0].HospitalId;
-    location.innerHTML = data.Items[0].Location;
-    Hospital_Name.innerHTML = data.Items[0].Hospital_Name;
-    Testing_Center.innerHTML = data.Items[0].Testing_Center;
-    Rating.innerHTML = data.Items[0].Rating;
+    
+
+    success: function(data){        
+        
+        $('#myTable > tbody:last-child').html('');
+
+        data.Items.forEach(element => {
+            $('#myTable > tbody:last-child').append('<tr>');
+            $('#myTable > tbody:last-child').append('<td>' +element.HospitalName+ '</td>');
+            $('#myTable > tbody:last-child').append('<td>' +element.Location+ '</td>');
+            $('#myTable > tbody:last-child').append('<td>' +element.County+ '</td>');
+            $('#myTable > tbody:last-child').append('<td>' +element.State+ '</td>');
+            $('#myTable > tbody:last-child').append('<td>' +element.OpenHours+ '</td>');
+            $('#myTable > tbody:last-child').append('<td>' +element.PhoneNumber+ '</td>');
+            $('#myTable > tbody:last-child').append('<td>' +element.Website+ '</td>');
+            /*for (const [key, value] of Object.entries(element)) {
+                if(key==='HospitalName' || key==='Location' || key==='OpenHours' || key==='PhoneNumber' || key==='Website')
+                    $('#myTable > tbody:last-child').append('<td>' +value+ '</td>');
+            }*/
+            $('#myTable > tbody:last-child').append('</tr>');
+        }) ;   
+    }
+});
+return false;
 });
